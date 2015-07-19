3bc321d665f012b5258d68939a8ff1db8c795b6d
Chaoyi Zha <summermontreal@gmail.com>
2015-07-19 02:50:41 -0400

    add qrcode generator

--- pp/utils/qrcode.py
+++ pp/utils/qrcode.py
@@ -1,18 +1,28 @@
 import pyqrcode
 import svgwrite
 import sys
-from io import BytesIO
-from io import FileIO
-import random
-import string
-import asyncio
-from threading import Thread
-from io import BufferedIOBase
-from pprint import pprint
+import os
 
-def qrfilegen():
-    qrfilename = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(100))
-    url = pyqrcode.QRCode()
+# from io import BytesIO
+import random, string
+
+def get_str():
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+
+def qrfilegen(link):
+    qrfilename = get_str()
+    correct = False
+    while correct != True:
+        relative = "qr-{}.svg".format(qrfilename)
+        gen_fn = "../static/svgs/{}".format(relative)
+        if os.path.isfile(gen_fn):
+            continue
+        else:
+            correct = True
+    url = pyqrcode.QRCode(link)
+    url.svg(gen_fn, scale=1, xmldecl=False)
+    return relative
     #handle = BytesIO()
     #url = pyqrcode.QRCode(qrcodeval)
     #url.svg(handle, scale = 2, xmldecl=False)
@@ -21,24 +31,29 @@ def qrfilegen():
     #return "".join(tup)
     #return handle
 
-def svgfilename():
-    fileio = FileIO('helllo')
-    svg_document = svgwrite.Drawing(filename = "qrcodegen.svg",size = ("1000px", "1000px"))
+def svgfilename(link):
+    svg_document = svgwrite.Drawing(filename = "qrcodegen.svg", size = ("1000px", "1000px"))
     svg_document.add(svg_document.image("paysign1.svg", insert = (0,0), size = ("35px", "35px")))
     svg_document.add(svg_document.image("paysign1.svg", insert = (0,200), size = ("35px", "35px")))
     svg_document.add(svg_document.image("paysign1.svg", insert = (300,0), size = ("35px", "35px")))
     svg_document.add(svg_document.image("paysign1.svg", insert = (300,200), size = ("35px", "35px")))
     svg_document.add(svg_document.image("paypyrus-sign.svg", insert = (55,5), size = ("300px", "300px")))
-    qrstr = qrfilegen("http:nonsesns")
-    fileio.write(qrstr)
-    svg_document.add(svg_document.image(fileio, insert=(80, 40), size=("250px", "250px")))
-    print(svg_document.tostring())
-    f = open('qrcodegenfinal.svg', 'w')
-    f.write(svg_document.tostring())
-    f.close()
-    return svg_document.tostring()
+    qrstr = qrfilegen(link)
+    svg_document.add(svg_document.image(qrstr, insert=(80, 40), size=("175px", "175px")))
+    string = svg_document.tostring()
+    correct = False
+    while correct != True:
+        qrfilename = get_str()
+        fn = "../static/svgs/final-{}.svg".format(qrfilename)
+        if os.path.isfile(fn):
+            continue
+        else:
+            correct = True
 
+    with open(fn, "w") as f:
+        f.write(svg_document.tostring())
 
+    return fn
 
-
-
+if __name__ == "__main__":
+    svgfilename("http://google.com")
